79af0723147acac4a7b09ea24a995123709a37cf
Anshul Lasod <Anshullasod@gmail.com>
2026-01-25 16:22:12 +0530

    Kth level using levelorder traversal {self solve}

---
+++ KthLEVEL.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+Node *maketree(vector<int> &roots, int &idx)
+{
+    idx++;
+    if (roots[idx] == -1)
+    {
+        return NULL;
+    }
+    Node *temp = new Node(roots[idx]);
+    temp->left = maketree(roots, idx);
+    temp->right = maketree(roots, idx);
+    return temp;
+}
+Node *leveltree(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            if (q.empty())
+            {
+                break;
+            }
+            cout << endl;
+            q.push(NULL);
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right != NULL)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+void Kthlevel(Node* root,int k)
+{  queue<Node*> q;    //Level,node
+   int l=1;
+   q.push(root);
+   q.push(NULL);
+   while(!q.empty())
+   {
+     Node* temp=q.front();
+    q.pop();
+    if(temp==NULL)
+    {
+        if(q.empty())
+        {  break;   }
+        l++;
+        q.push(NULL);
+    }
+    else{
+    if(l==k)
+    { cout<<temp->data<<" "; }
+    if(temp->left!=NULL)
+    {
+        q.push(temp->left);
+    }
+     if(temp->right!=NULL)
+    {
+        q.push(temp->right);
+    } } }}
+int main()
+{
+    vector<int> roots={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    int idx=-1;
+    Node* root=maketree(roots,idx);
+    leveltree(root);
+    cout<<endl;
+    Kthlevel(root,2);
+}
