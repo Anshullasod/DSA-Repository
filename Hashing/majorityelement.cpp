65580b900aad4682e6d8c5a95e68fe0d53c18367
Anshul Lasod <Anshullasod@gmail.com>
2026-02-10 17:21:55 +0530

    easy done

---
+++ Hashing/majorityelement.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+void majorityelement(vector<int> &nums)
+{
+    unordered_map<int,int> m;
+    for(int i=0;i<nums.size();i++)
+  {
+    if(m.find(nums[i])!=m.end())
+    {
+        m[nums[i]]++;
+    }
+    else
+    {
+        m[nums[i]]=1;
+    }
+  }
+  for(pair<int,int> p:m)
+  {
+    if(p.second>nums.size()/3)
+    {   
+        cout<<p.first<<" ";
+    }
+  }
+  return;}
+int main()
+{
+    vector<int> nums={1,9,9,9,3};
+    majorityelement(nums);
+}
