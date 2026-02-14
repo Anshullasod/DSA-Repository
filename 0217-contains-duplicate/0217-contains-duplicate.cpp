9018ea9f97084d37bfa69c2dc13c48f404b20b3f
Anshul Lasod <anshullasod@gmail.com>
2026-02-15 00:03:18 +0530

    Time: 51 ms (48.49%), Space: 90.9 MB (43.71%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> m; //num,count
+        unordered_set<int> m; 
         for(int i=0;i<nums.size();i++)
         {
             if(m.find(nums[i])!=m.end())
             {
                 return true;
             }
-            m[nums[i]]=1;
+            m.insert(nums[i]);
         }
         return false;
     }
