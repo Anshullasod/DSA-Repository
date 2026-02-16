3b3fd494157a02d7b8667f58f99df09b90359cb8
Anshul Lasod <Anshullasod@gmail.com>
2026-02-16 16:13:48 +0530

    refreshed

--- Hashing/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ Hashing/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-         unordered_map<int,int> m;
-    int ans=0;
-    int sum=0;
-    m[0]=1;
-    for(int i=0;i<nums.size();i++)
-    {
+      unordered_map<int,int> m;  //currsum,count
+      int sum=0;
+      int count=0;
+      m[0]=1;
+      for(int i=0;i<nums.size();i++)
+      {
         sum+=nums[i];
-       if(m.count(sum-k))
-       {
-         ans+=m[sum-k];
-       }
-       if(m.count(sum))
-       {
+        if(m.count(sum-k))
+        {
+            count+=m[sum-k];
+        }
         m[sum]++;
-       }
-       else
-       {
-        m[sum]=1;
-       }
-    } 
-    return ans;
+      }
+      return count;
     }
 };
