1ec7bb37a0c7a774b430080f39283a1a724e156d
Anshul Lasod <anshullasod@gmail.com>
2026-02-18 11:48:14 +0530

    Time: 58 ms (49.44%), Space: 84.1 MB (16.88%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
-class Point{
-  public:
-  int hd;
-  int vd;
-  Point(int hd,int vd)
-  {
-    this->hd=hd;
-    this->vd=vd;
-  }
-  long int dist()
-  {
-    return (hd*hd)+(vd*vd);
-  }
-};
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<long int,vector<int>>> pq;
-  for(int i=0;i<points.size();i++)
-  {
-    Point temp(points[i][0],points[i][1]);
-    long int distance=temp.dist();
-    if(pq.size()<k)
-    {
-      pq.push({distance,{points[i][0],points[i][1]}});
-    }
-    else
-    { if(distance<pq.top().first)
-      {pq.pop();
-      pq.push({distance,{points[i][0],points[i][1]}});
-    }}
-  }
-  vector<vector<int>> ans;
-  while(!pq.empty())
-  {
-    ans.push_back(pq.top().second);
-    pq.pop();
-  }
-  for(int i=0;i<ans.size();i++)
-  {
-    cout<<ans[i][0]<<","<<ans[i][1]<<endl;
-  }
- return ans;
-    }
+       priority_queue<pair<int,vector<int>>,vector<pair<int,vector<int>>>,greater<pair<int,vector<int>>>> p;       
+       for(int i=0;i<points.size();i++)
+       {
+        int dist=points[i][0]*points[i][0]+points[i][1]*points[i][1];
+        p.push({dist,points[i]});
+       }    
+       int i=0;
+       vector<vector<int>> ans;
+       while(i!=k)
+       {
+        i++;
+    ans.push_back(p.top().second);
+  
+        p.pop();
+       }
+       return ans;
+       }
 };
