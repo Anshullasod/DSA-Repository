7c68ba7a661623ffa4b92d623237a2ff58838ff1
Anshul Lasod <Anshullasod@gmail.com>
2026-02-19 15:28:55 +0530

    done

--- Heaps/0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ Heaps/0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,32 +1,32 @@
 class MedianFinder {
 public:
-priority_queue<int> maxpq; //Left
-priority_queue<int,vector<int>,greater<int>> minpq; //Right
+  priority_queue<int> pq1; //Max heap for first half;
+  priority_queue<int,vector<int>,greater<int>> pq2;//<in heap for second half
     MedianFinder() {
-         
+        
     }
-    
-    void addNum(int num) {
-     maxpq.push(num);
-     minpq.push(maxpq.top());
-     maxpq.pop();
-     if(maxpq.size()<minpq.size())
-     {
-        maxpq.push(minpq.top());
-        minpq.pop();
-     }
+  void addNum(int num)
+  {
+    pq1.push(num);
+    pq2.push(pq1.top());
+    pq1.pop();
+    if(pq1.size()<pq2.size())
+    {
+        pq1.push(pq2.top());
+        pq2.pop();
     }
-    
-    double findMedian() {
-        int maxsize=maxpq.size();
-        int minsize=minpq.size();
-        if((maxsize+minsize)%2!=0)
-        {
-         return maxpq.top();
-        }
-        double median=(minpq.top()+maxpq.top())/2.0;
-        return median;
+    return;
+  }
+  double findMedian()
+  { 
+    if(pq1.size()==pq2.size())
+    {double median=(pq1.top()+pq2.top())/2.0;
+    return median;}
+    else{
+      double num=pq1.top();
+    return num;
     }
+  }
 };
 
 /**
